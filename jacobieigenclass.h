ac43274ae6fc32bc5690c09b5420d63ab05b1b31
D. Wolf <forecastdesigner@gmail.com>
2020-02-05 20:16:55 +0300

    Update jacobieigenclass.h

--- jacobieigenclass.h
+++ jacobieigenclass.h
@@ -3,7 +3,6 @@ The C++ class of the classical Jacobi's method implementation for finding eigenv
 eigenvectors of a symetric matrix which represented as 1-dimension array.
 The eigenvalues of a symetric matrix will be on the array eigenVals of the class JacobiEigensClass.
 The i-th eigenvector is stored in array eigenVecs of the class JacobiEigensClass.
-
 originVector: input array (N x N)
 eigenVecs: array for eigenvectors (N x N)
 N: dimention of matrices
@@ -15,31 +14,31 @@ N: dimention of matrices
 #define JACOBIEIGENCLASS_H
 
 class JacobiEigensClass {
-    
-    private:
-        double epsilon;
-        int N;
-        int maxIterations;
-        int totalIterations;
-        int currentIteration;
-        int currentIndexOfMaxElement;
-        double currentMaxElement;
 
-        double *varVector;
-        //double *rotVector;
-    
-    public:
-        double *originVector = NULL;    //input, original matrix in array representation
-        double *eigenVecs = NULL;       //output array with eigenvalues
-        double *eigenVals = NULL;       //output array with eigenvector
+private:
+    const double epsilon;
+    const int N;
+    int maxIterations;
+    int totalIterations;
+    int currentIteration;
+    int currentIndexOfMaxElement;
+    double currentMaxElement;
 
-        JacobiEigensClass(std::vector<double> inputMatrix, double epsilon);
-        ~JacobiEigensClass();
-        void calculateEigensByJacobiMethod();
-    
-    private:
-        void findMaxElementInVarVector();
-        void rotateVarVector();
+    double* varVector;
+    //double *rotVector;
+
+public:
+    double* originVector = NULL;    //input, original matrix in array representation
+    double* eigenVecs = NULL;       //output array with eigenvalues
+    double* eigenVals = NULL;       //output array with eigenvector
+
+    JacobiEigensClass(const std::vector<double> &inputMatrix, double epsilon);
+    ~JacobiEigensClass();
+    void calculateEigensByJacobiMethod();
+
+private:
+    void findMaxElementInVarVector();
+    void rotateVarVector();
 
 };
 
